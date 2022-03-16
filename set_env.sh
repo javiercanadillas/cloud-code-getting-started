d4ee6f2a93e4d0eee798a3a32b050d02f0c7491d
Javier Cañadillas <javiercm@google.com>
2022-03-16 23:13:28 +0100

    Cleaned EOF

--- set_env.sh
+++ set_env.sh
@@ -7,11 +7,11 @@ SCRIPTS_PATH="$HOME/cloud-code-getting-started/scripts"
 
 # Copy Cloud Shell customization and launch it
 cp "$SCRIPTS_PATH/$CUST_NAME" "$HOME"
-# shellcheck source=.customize_environment
+# shellcheck source=/dev/null
 source "${HOME}/${CUST_NAME}"
 
 # Modify bashrc to include user-based customizations
 cp "$SCRIPTS_PATH/$EXTRA_BASHRC" "$HOME/$EXTRA_BASHRC"
 cat <<EOF >> $HOME/.bashrc
 source "$HOME/$EXTRA_BASHRC"
-EOF 
+EOF
